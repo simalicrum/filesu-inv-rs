63a200070afecdcbecd1a3c27d6809b3c44811e0
Robert Reinert <robert.reinert@gmail.com>
2023-06-01 01:16:47 +0000

    app does sequential fetches on marker

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,31 @@ use serde::{Deserialize, Serialize};
 use serde_xml_rs::{from_str, to_string};
 use std::error::Error;
 use std::io::BufRead;
+use std::marker;
 use url::Url;
 
 async fn list_blobs(
     container: &str,
     account: &str,
     token: &str,
+    marker: Option<&str>,
     client: &reqwest::Client,
 ) -> Result<String, Box<dyn Error>> {
-    let url = Url::parse(&format!(
-        "https://{}.blob.core.windows.net/{}?restype=container&comp=list&maxresults=5",
-        account, container,
-    ))?;
+    let url;
+    match marker {
+        None => {
+            url = Url::parse(&format!(
+                "https://{}.blob.core.windows.net/{}?restype=container&comp=list",
+                account, container,
+            ))?
+        }
+        Some(m) => {
+            url = Url::parse(&format!(
+                "https://{}.blob.core.windows.net/{}?restype=container&comp=list&marker={}",
+                account, container, m,
+            ))?;
+        }
+    };
     let res = client
         .get(url)
         .header("Authorization", format!("Bearer {}", token))
@@ -63,7 +76,6 @@ fn read_to_end_into_buffer<R: BufRead>(
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(rename_all = "PascalCase")]
 struct Properties {
     #[serde(rename = "Creation-Time")]
     creation_time: String,
@@ -88,6 +100,11 @@ struct Blob {
     #[serde(rename = "Properties")]
     properties: Properties,
 }
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+struct NextMarker {
+    #[serde(rename = "NextMarker")]
+    marker: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -97,30 +114,48 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let client = reqwest::Client::new();
     let container = "atlas";
     let account = "bccrcprccatlassa";
-    let res = list_blobs(container, account, token, &client).await?;
-    let mut reader = Reader::from_str(&res);
-    reader.trim_text(true);
-    let mut count = 0;
-    let mut buf = Vec::new();
-    // let mut txt = Vec::new();
-    let mut junk_buf: Vec<u8> = Vec::new();
-    loop {
-        match reader.read_event_into(&mut buf) {
-            Ok(Event::Start(e)) => match e.name().as_ref() {
-                b"Blob" => {
-                    let release_bytes =
-                        read_to_end_into_buffer(&mut reader, &e, &mut junk_buf).unwrap();
-                    let str = std::str::from_utf8(&release_bytes).unwrap();
-                    let blob: Blob = from_str(str).unwrap();
-                    println!("{:?}", blob);
-                }
+    let mut marker: Option<&str> = None;
+    let mut next_marker: String;
+    'list: loop {
+        // println!("Starting loop");
+        let res = list_blobs(container, account, token, marker, &client).await?;
+        let mut reader = Reader::from_str(&res);
+        reader.trim_text(true);
+        let mut count = 0;
+        let mut buf = Vec::new();
+        // let mut txt = Vec::new();
+        let mut junk_buf: Vec<u8> = Vec::new();
+        loop {
+            match reader.read_event_into_async(&mut buf).await {
+                Ok(Event::Start(e)) => match e.name().as_ref() {
+                    b"Blob" => {
+                        let release_bytes =
+                            read_to_end_into_buffer(&mut reader, &e, &mut junk_buf).unwrap();
+                        let str = std::str::from_utf8(&release_bytes).unwrap();
+                        let blob: Blob = from_str(str).unwrap();
+                        // println!("{:?}", blob);
+                    }
+                    b"NextMarker" => {
+                        let release_bytes =
+                            read_to_end_into_buffer(&mut reader, &e, &mut junk_buf).unwrap();
+                        let str = std::str::from_utf8(&release_bytes).unwrap();
+                        next_marker = from_str(str).unwrap();
+                        if next_marker.is_empty() {
+                            println!("No more markers");
+                            break 'list;
+                        }
+                        marker = Some(&next_marker);
+                        // println!("Next marker: {:?}", marker);
+                    }
+                    _ => (),
+                },
+                Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                Ok(Event::Eof) => break,
                 _ => (),
-            },
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
-            Ok(Event::Eof) => break,
-            _ => (),
+            }
+            buf.clear();
         }
-        buf.clear();
     }
+
     Ok(())
 }
